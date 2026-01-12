480f18f6c9500b5fc7239c232438b568bc01e063
davinazka <irsanteluma@gmail.com>
2026-01-12 09:56:04 +0700

    Tambah nilai siswa

--- Program.cs
+++ Program.cs
@@ -7,3 +7,12 @@ Console.Write("Input nama siswa");
 nama = Console.ReadLine();
 Console.Write("Input nilai siswa");
 nilai = int.Parse (Console.ReadLine());
+
+if (nilai >= 75)
+{
+    Console.WriteLine("Siswa lulus");
+}
+else
+{
+    Console.WriteLine("Siswa tidak lulus yey");
+}
